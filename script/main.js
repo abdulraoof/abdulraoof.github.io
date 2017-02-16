6e2ab49ad8ad89419937bc42d0894e766fcfe9aa
abdulraoof <mailtoraoof@gmail.com>
2017-02-16 23:31:04 +0530

    added quotes section

--- script/main.js
+++ script/main.js
@@ -1,3 +1,10 @@
-console.log("working fine");
+$( document ).ready(function() {
+	var quotes = $('blockquote');
+	for(var i = 0; i < quotes.length; i++){
+    var target1 = Math.floor(Math.random() * quotes.length -1) + 1;
+    var target2 = Math.floor(Math.random() * quotes.length -1) +1;
+    quotes.eq(target1).before(quotes.eq(target2));
+	}
+});
 
 
